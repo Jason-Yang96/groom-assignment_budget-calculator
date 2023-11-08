d98c145f9a697247b05e32c272a994137cbf66f6
Jason Yang <didwotjs120@gmail.com>
2023-11-08 22:21:00 +0900

    under spliting components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import './styles/App.css';
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import Header from './components/Header';
+import Notification from './components/Notification';
+import Form from './components/Form';
+import Sum from './components/Sum';
 
 const App = () => {
 	const [expenseData, setExpenseData] = useState([]);
 	const [expenseName, setExpenseName] = useState('');
 	const [expenseValue, setExpenseValue] = useState('');
-	const [alerMessage, setAlerMessage] = useState(null);
+	const [alertMessage, setAlertMessage] = useState(null);
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -19,10 +23,9 @@ const App = () => {
 		setExpenseData((prev) => [...prev, newExpense]);
 		setExpenseName('');
 		setExpenseValue('');
-		setAlerMessage('항목이 추가되었습니다');
-		setTimeout(() => setAlerMessage(null), 2000);
+		setAlertMessage('항목이 추가되었습니다');
+		setTimeout(() => setAlertMessage(null), 2000);
 	};
-
 	const handleExpenseNameChange = (e) => {
 		setExpenseName(e.target.value);
 	};
@@ -32,14 +35,10 @@ const App = () => {
 	const handleRemove = (id) => {
 		let newExpenseData = expenseData.filter((data) => data.id !== id);
 		setExpenseData(newExpenseData);
-		setAlerMessage('항목이 삭제되었습니다');
-		setTimeout(() => setAlerMessage(null), 2000);
+		setAlertMessage('항목이 삭제되었습니다');
+		setTimeout(() => setAlertMessage(null), 2000);
 	};
-	const handleRomoveAllClick = () => {
-		setExpenseData([]);
-		setAlerMessage('항목이 모두 삭제되었습니다');
-		setTimeout(() => setAlerMessage(null), 2000);
-	};
+
 	const handleEnd = (e) => {
 		if (!e.destination) return;
 
@@ -50,115 +49,82 @@ const App = () => {
 	};
 	return (
 		<div className='relative flex flex-col items-center justify-center w-screen h-screen bg-blue-100'>
-			<div className='absolute top-11'>{alerMessage}</div>
+			<Notification alertMessage={alertMessage} />
 			<div className='bg-white w-full p-6 m-4 rounded shadow md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg'>
-				<header className='flex justify-between mb-3'>
-					<h1 className='text-2xl font-semibold	'>예산 계산기</h1>
-					<button onClick={handleRomoveAllClick}>Delete All</button>
-				</header>
-				<form
-					className='flex justify-between'
-					onSubmit={handleSubmit}>
-					<div className='flex flex-col flex-1'>
-						<label>지출 항목</label>
-						<input
-							type='text'
-							placeholder='항목 이름을 적어주세요'
-							onChange={handleExpenseNameChange}
-							value={expenseName}
-						/>
-					</div>
-					<div className='flex flex-col flex-1'>
-						<label>예상 비용</label>
-						<input
-							type='number'
-							min='500'
-							placeholder='예상 지출액을 적어주세요'
-							onChange={handleExpenseValueChange}
-							value={expenseValue}
-						/>
-					</div>
-					<input
-						className='flex-none'
-						type='submit'
-						value='추가'
-					/>
-				</form>
-				<DragDropContext onDragEnd={handleEnd}>
-					<Droppable droppableId='expenseItems'>
-						{(provided) => (
-							<div
-								{...provided.droppableProps}
-								ref={provided.innerRef}>
-								<ul className='mt-3'>
+				<Header
+					setExpenseData={setExpenseData}
+					setAlerMessage={setAlertMessage}
+				/>
+				<Form
+					handleSubmit={handleSubmit}
+					handleExpenseNameChange={handleExpenseNameChange}
+					expenseName={expenseName}
+					handleExpenseValueChange={handleExpenseValueChange}
+					expenseValue={expenseValue}
+				/>
+				<div>
+					<DragDropContext onDragEnd={handleEnd}>
+						<Droppable droppableId={'expenseItems'}>
+							{(provided) => (
+								<ul
+									{...provided.droppableProps}
+									ref={provided.innerRef}
+									className='mt-3'>
 									{expenseData.map((data, index) => (
 										<Draggable
 											key={data.id}
 											draggableId={data.id.toString()}
 											index={index}>
 											{(provided, snapshot) => (
-												<div
+												<li
 													key={data.id}
 													{...provided.draggableProps}
 													ref={provided.innerRef}
+													{...provided.dragHandleProps}
 													className={`${
 														snapshot.isDragging
 															? 'bg-gray-400'
 															: 'bg-gray-100'
-													}`}>
-													<li
-														className='flex justify-between mb-2 hover:bg-gray-200 active:bg-gray-300'
-														key={data.id}>
-														<span className='flex-1'>
-															{data.name}
-														</span>
-														<span className='flex-1'>
-															{parseInt(
-																data.value
-															).toLocaleString()}
-															원
-														</span>
-														<button
-															className='flex-none mr-2'
-															onClick={() =>
-																handleRemove(
-																	data.id
-																)
-															}>
-															수정
-														</button>
-														<button
-															className='flex-none'
-															onClick={() =>
-																handleRemove(
-																	data.id
-																)
-															}>
-															삭제
-														</button>
-													</li>
-												</div>
+													} flex justify-between mb-2 hover:bg-gray-200 active:bg-gray-300`}>
+													<span className='flex-1'>
+														{data.name}
+													</span>
+													<span className='flex-1'>
+														{parseInt(
+															data.value
+														).toLocaleString()}
+														원
+													</span>
+													<button
+														className='flex-none mr-2'
+														onClick={() =>
+															handleRemove(
+																data.id
+															)
+														}>
+														수정
+													</button>
+													<button
+														className='flex-none'
+														onClick={() =>
+															handleRemove(
+																data.id
+															)
+														}>
+														삭제
+													</button>
+												</li>
 											)}
 										</Draggable>
 									))}
+									{provided.placeholder}
 								</ul>
-							</div>
-						)}
-					</Droppable>
-				</DragDropContext>
+							)}
+						</Droppable>
+					</DragDropContext>
+				</div>
 			</div>
-			<div>
-				<h1>
-					총 예상 지출액:{' '}
-					{expenseData
-						.reduce(
-							(total, data) => total + parseInt(data.value),
-							0
-						)
-						.toLocaleString()}
-					원 {/* 1000 마다 구분자 넣어주기 */}
-				</h1>
-			</div>
+			<Sum expenseData={expenseData} />
 		</div>
 	);
 };
